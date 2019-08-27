a212a3b0b85c5f678a93d33ffa5247ddb8ca8a33
Kevin Tsai <k.tsai92@gmail.com>
2019-08-27 14:23:06 -0400

    second commit

--- server.js
+++ server.js
@@ -1,42 +1,62 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-
-console.log(process.env.API_TOKEN)
+const POKEDEX = require('./pokedex.json')
 
 const app = express()
 
-app.use(morgan('dev'))
-
-const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 
 app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken= req.get('Authorization')
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
 
-    console.log('validate bearer token middleware')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
 
-    if(!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401).json({ error: 'Unauthorized request'})
-    }
-
-    next()
+  next()
 })
 
-function handleGetTypes(req, res) {
-    res.json(validTypes)
-}
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychich`, `Rock`, `Steel`, `Water`]
 
-app.get('/types', handleGetTypes)
+app.get('/types', function handleGetTypes(req, res) {
+  res.json(validTypes)
+})
 
-function handleGetPokemon(req, res) {
-    res.send('Hello, Pokemon!')
-}
+app.get('/pokemon', function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
 
-app.get('/pokemon', handleGetPokemon)
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    )
+  }
 
-const PORT = 8000
+  
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    )
+  }
+
+  res.json(response)
+})
+
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://locahost:${PORT}`)
+  console.log(`Server listening at http://localhost:${PORT}`)
 })
